8009344bea80aae8f7be4fb26b8c0227cc931ca0
ananthu <ask1234560@gmail.com>
2020-04-08 22:46:48 +0530

    initial

---
+++ zjumper_ranger.py
@@ -0,0 +1,28 @@
+import ranger.api
+import subprocess
+from ranger.api.commands import *
+
+
+class z(Command):
+    """:z
+    Uses z to set the current directory.
+    """
+    def execute(self):
+
+        # return directories satisfying the querry
+        #  ~/.z file to be present in home, if file in different location change the below command at the end
+        cmd = """awk -F "|" -v q=\""""+" ".join(self.args[1:])+"""\" 'BEGIN{gsub(/ /, ".*", q)}  { if(tolower($1) ~ q){ print $1} }' < ~/.z"""
+
+
+        directories,error = subprocess.Popen(cmd, shell=True, executable="/bin/bash", stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+        directories = directories.decode("utf-8", "ignore")
+        directories = directories.rstrip('\n').split("\n")
+
+        #  smallest directory will be the directory required
+        sl,os = len(directories[0]),directories[0]
+        for directory in directories[1:]:
+            l = len(directory)
+            if l < sl:
+                sl = l;
+                os = directory;
+        self.fm.execute_console("cd " + os)
